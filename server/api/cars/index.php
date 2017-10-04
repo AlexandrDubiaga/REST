99ead893916cfe60d2e8d0764128019e68350744
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-04 19:19:17 +0300

    Update index.php

--- server/api/cars/index.php
+++ server/api/cars/index.php
@@ -9,6 +9,7 @@ class Cars extends RestServer
 
     public function getCars($data = false)
     {
+        echo "Test" .$data;
          $link = mysqli_connect('localhost', 'user2', 'tuser2', 'user2');
         mysqli_set_charset($link,'utf8');
          $result = mysqli_query($link, "SELECT * FROM AutoShop");
