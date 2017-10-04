fe028ca4bafe8b36c6ec5c398b0676ef06aa6d1a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-04 19:14:29 +0300

    Update index.php

--- server/api/cars/index.php
+++ server/api/cars/index.php
@@ -19,5 +19,29 @@ class Cars extends RestServer
        
 
     }
+     public function postCars($data = false)
+    {
+           $id = $_POST['id'];
+           $marka = $_POST['marka'];
+           $model = $_POST['model'];
+           $year = $_POST['year_car'];
+           $engine = $_POST['engine_capacity'];
+           $speed = $_POST['max_speed'];
+           $price = $_POST['price'];
+           $link = mysqli_connect('localhost', 'user2', 'tuser2', 'user2');
+           mysqli_set_charset($link,'utf8');
+           $result = mysqli_query($link, "INSERT into AutoShop VALUES ($id,$marka,$model,$year,$engine,$speed,$price)");
+            if($result)
+            {
+                return true;
+            }
+         else
+         {
+            return false;
+         }
+
+       
+
+    }
 }
 $cars = new Cars();
