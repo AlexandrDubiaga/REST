28dd1317e61708d7d4cf7ca08961dae0423fefc8
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-05 20:34:42 +0300

    Update index.php

--- server/api/cars/index.php
+++ server/api/cars/index.php
@@ -17,8 +17,8 @@ class Cars extends RestServer
         $result = mysqli_query($this->link, "SELECT * FROM AutoShop");
         while ($row[] = mysqli_fetch_array($result, MYSQL_ASSOC)) {
         }
-       
-       print_r($this->encodedData($row));
+       $ad = array('sefse'=>'sefsef','awdawrsfg'=>'sefreg4','rgrtgrt'=>'drgdrg');
+       print_r($this->encodedData($ad));
      
 
        
