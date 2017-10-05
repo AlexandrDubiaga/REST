7a549bdb7b9ff43b4e8a734b8bd0c720aeaa1c1a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-05 20:26:30 +0300

    Update RestServer.php

--- server/app/RestServer.php
+++ server/app/RestServer.php
@@ -58,11 +58,7 @@ class RestServer
                 return  $this->convertToHtml($data);
                 break;
             case '.xml':
-                  header("Content-type: text/xml");
-                $xml = new SimpleXMLElement('<root/>');
-                $data = array_flip($data);
-                array_walk_recursive($data, array($xml, 'addChild'));
-               print_r($xml->asXML());
+                 return $this->convertToXml($data);
                 break;
             default:
                  return  $this->convertToJson($data);
