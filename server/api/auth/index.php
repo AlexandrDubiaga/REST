de324ff086552f6235e549a9651c7b0da634da8e
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-06 18:53:22 +0300

    Update index.php

--- server/api/auth/index.php
+++ server/api/auth/index.php
@@ -15,8 +15,60 @@ class Auth extends RestServer
     }
     protected function postAuth()
     {
+        
+    $query = mysqli_query( $this->link,"SELECT user_id, user_password FROM users WHERE user_login='".$_POST['login']."' LIMIT 1");
+
+    $data = mysqli_fetch_assoc($query);
+
+    
+
+    # Соавниваем пароли
+
+    if($data['user_password'] === md5(md5($_POST['password'])))
+
+    {
+
+        # Генерируем случайное число и шифруем его
+
+        $hash = md5(generateCode(10));
+        mysql_query("UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
+        setcookie("id", $data['user_id'], time()+60*60*24*30);
+
+        setcookie("hash", $hash, time()+60*60*24*30);
+
+        
+
+        # Переадресовываем браузер на страницу проверки нашего скрипта
+
+        header("Location: check.php"); exit();
+
+    }
+
+    else
+
+    {
+
+        print "Вы ввели неправильный логин/пароль";
+
+    }
       
     }
+    function generateCode($length=6) {
+
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+
+    $code = "";
+
+    $clen = strlen($chars) - 1;  
+    while (strlen($code) < $length) {
+
+            $code .= $chars[mt_rand(0,$clen)];  
+    }
+
+    return $code;
+
+}
+
 }
 $auth = new Auth();
 ?>
