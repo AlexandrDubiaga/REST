0b2ca221eb67f5823a3d9d58945e13b947420fbc
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-06 17:31:18 +0300

    Update index.php

--- server/api/users/index.php
+++ server/api/users/index.php
@@ -29,7 +29,7 @@ class Users extends RestServer
         {
             $err[] = "login !< 3 and !> 30 letters";
         }
-        $query = mysql_query("SELECT COUNT(user_id) FROM users WHERE user_login='".mysql_real_escape_string( $_POST['user_login'])."'");
+        /*$query = mysql_query("SELECT COUNT(user_id) FROM users WHERE user_login='".mysql_real_escape_string( $_POST['user_login'])."'");
         if(mysql_result($query, 0) > 0)
         {
             $err[] = "This login is isset allredy";
@@ -49,7 +49,7 @@ class Users extends RestServer
                 print $error."<br>";
             }
 
-        }
+        }*/
 
     }
 
