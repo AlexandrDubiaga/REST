8b5a54f0e7e3689392084d4a6aff6e814eedb73d
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-06 17:17:22 +0300

    Update DB.php

--- server/app/DB.php
+++ server/app/DB.php
@@ -5,7 +5,7 @@ class DB
     protected $dBMain;
     public function __construct()
     {
-        $this->dBMain = mysqli_connect(HOST, USER, PASSWORD, DB);
+        $this->dBMain = mysqli_connect('localhost', 'user2', 'tuser2', 'user2');
         mysqli_set_charset( $this->dBMain,'utf8');
     }
 }
