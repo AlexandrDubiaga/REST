b8992f44a30246affb50c5376bef848ed13a8343
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-06 19:37:06 +0300

    Update index.php

--- server/api/check/index.php
+++ server/api/check/index.php
@@ -11,6 +11,7 @@ class Check extends RestServer
     }
     protected function getAuth($data)
     {
+          echo "Cockies";
           var_dump($_COOKIE['id']);
        if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
 
