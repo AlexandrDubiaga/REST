a450faeff1a1b403069cb5783f71ac9bd78a54fe
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-09 19:31:10 +0300

    Update DB.php

--- server/app/DB.php
+++ server/app/DB.php
@@ -5,7 +5,8 @@ class DB
     protected $dBMain;
     public function __construct()
     {
-        $this->dBMain = new PDO('mysql:host=10.3.149.74;dbname=work', 'bti', 'bti');
+       // $this->dBMain = new PDO('mysql:host=10.3.149.74;dbname=work', 'bti', 'bti');
+        $this->dBMain = new PDO('mysql:host=localhost;dbname=user2', 'tuser2', 'user2');
         if (!$this->dBMain)
         {
             throw new PDOException("Error db");
