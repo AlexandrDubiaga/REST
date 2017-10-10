6ce7ea60fe3fd0a2340241af20fb101c2beacf90
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-10 20:13:17 +0300

    Update ModelCars.php

--- server/app/models/ModelCars.php
+++ server/app/models/ModelCars.php
@@ -10,14 +10,30 @@ class ModelCars extends RestServer
     }
     public function getCars($param=false)
     {
+         $sql = "SELECT car_id, marka, model, year_car, 	engine_capacity, color, max_speed, price FROM cars";
+        if ($param !== false){
+            if (is_array($param))
+            {
+                $sql .= " WHERE ";
+                foreach ($param as $key => $val)
+                {
+                    $sql .= $key.'='.$this->link->quote($val).' AND ';
+                }
+                $sql = substr($sql, 0, -5);
+               }
+            $sth = $this->link->prepare($x);
+            $result = $sth->execute();
+            $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        }
         
-             $x = "SELECT car_id, marka, model, year_car, engine_capacity, color, max_speed, price FROM cars";
+           /*  $x = "SELECT car_id, marka, model, year_car, engine_capacity, color, max_speed, price FROM cars";
             $sth = $this->link->prepare($x);
             $result = $sth->execute();
             $res = $sth->fetchAll(PDO::FETCH_ASSOC);
          
       
-           return $res;
+           return $res;*/
         
         
       //  $sql = "SELECT car_id, marka, model, year_car, 	engine_capacity, color, max_speed, price FROM cars";
