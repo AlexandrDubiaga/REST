62fdabc23465f8d6647a43cc68772bf7104b35c9
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-10 20:08:04 +0300

    Update ModelCars.php

--- server/app/models/ModelCars.php
+++ server/app/models/ModelCars.php
@@ -10,7 +10,7 @@ class ModelCars extends RestServer
     }
     public function getCars($param=false)
     {
-        if(!param)
+        if(!$param)
         {
              $x = "SELECT car_id, marka, model, year_car, engine_capacity, color, max_speed, price FROM cars";
             $sth = $this->link->prepare($x);
