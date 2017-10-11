23f1454c1deef3b1040a590e59b3cbb41f357d00
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-10-11 19:33:05 +0300

    Update ModelCars.php

--- server/app/models/ModelCars.php
+++ server/app/models/ModelCars.php
@@ -11,7 +11,7 @@ class ModelCars extends RestServer
     public function getCars($param=false)
     {
          
-        if ($param !== false)
+      /*  if ($param !== false)
         {
             $sql = "SELECT car_id, marka, model, year_car, engine_capacity, color, max_speed, price FROM cars";
           
@@ -31,15 +31,15 @@ class ModelCars extends RestServer
         }
         return $data;
         }elseif($param === false)
-        {
+        {*/
              $sql = "SELECT car_id, marka, model, year_car, engine_capacity, color, max_speed, price FROM cars";
               $sth = $this->link->prepare($sql);
                $result = $sth->execute();
              $data = $sth->fetchAll(PDO::FETCH_ASSOC);
                return $data;
         
-        
-        }
+      /*  
+        }*/
     }
 
     public function postCars($data)
